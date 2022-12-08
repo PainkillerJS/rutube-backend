8b3042a6b87b169dd688053530ab879ac8e06e40
Yuriy <nikorezar@gmail.com>
2022-12-08 23:21:40 +0300

    feat: controller of auth had added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
 
+import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() authDto: AuthDto) {
+		return this.authService.login(authDto);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('register')
+	async register(@Body() authDto: AuthDto) {
+		return this.authService.register(authDto);
+	}
 }
